53e3ce0b2688da1465eb6f8d94e7b2811f02ad16
Mohima Ahmed Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2024-10-26 22:03:07 +0600

    Create move_zeroes.cpp

---
+++ move_zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int non_zero = 0;
+        
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != 0) nums[non_zero++] = nums[i];
+        
+        for (int i = non_zero; i < nums.size(); i++)
+            nums[i] = 0;
+    }
+};
