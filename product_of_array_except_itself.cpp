078be66200a6b6a90c3e7739812cf90f8bbcbe58
Mohima Ahmed Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2024-10-26 12:40:57 +0600

    Create product_of_array_except_itself.cpp

---
+++ product_of_array_except_itself.cpp
@@ -0,0 +1,26 @@
+// couldn't solve it without the division operation in O(n)
+//in brute force it's O(n^2) and using division method it's O(n)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int count_zero = 0, product = 1;
+
+        for(int num : nums) {
+            if(num == 0) count_zero++;
+            else product *= num;
+        }
+
+        if(count_zero > 1) {
+            return vector<int> (nums.size(), 0);
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(count_zero > 0)
+                result[i] = (nums[i] == 0) ? product : 0;
+            else result[i] = product / nums[i];
+        }
+        return result;
+    }
+};
