4b72df09ced74159de210e7ebe1f310c09711228
Mohima Ahmed Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2024-10-26 22:17:27 +0600

    Create h-index.cpp

---
+++ h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size(), h;
+      
+        for (int i = 0; i < n; ++i) {
+            h = n - i;  
+            if (citations[i] >= h) return h;
+        }
+        return 0;
+    }
+};
