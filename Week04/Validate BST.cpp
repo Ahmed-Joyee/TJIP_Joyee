86656e750de2c3642ddb51b457bd3328a01e9073
Mohima Ahmed Joyee <82671884+Ahmed-Joyee@users.noreply.github.com>
2024-12-15 21:28:07 +0600

    Create Validate BST.cpp

---
+++ Week04/Validate BST.cpp
@@ -0,0 +1,31 @@
+//TC: O(n)
+//MC: O(n)
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+
+        queue<tuple<TreeNode*, long, long>> queue;
+        queue.push(make_tuple(root, LONG_MIN, LONG_MAX));
+
+        while (!queue.empty()) {
+            auto [node, left, right] = queue.front();
+            queue.pop();
+
+            if (!(left < node->val && node->val < right)) {
+                return false;
+            }
+            if (node->left) {
+                queue.push(make_tuple(node->left, left, node->val));
+            }
+            if (node->right) {
+                queue.push(make_tuple(node->right, node->val, right));
+            }
+        }
+
+        return true;
+    }
+};
